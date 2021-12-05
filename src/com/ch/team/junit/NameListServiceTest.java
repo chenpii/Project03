647eec027d5c2efc2e440f16fa3dbbf6ac3898e4
chenh <641760512@qq.com>
2021-12-05 17:10:08 +0800

    获取单个员工信息方法的单元测试

--- src/com/ch/team/junit/NameListServiceTest.java
+++ src/com/ch/team/junit/NameListServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import com.ch.team.domain.Employee;
 import com.ch.team.service.NameListService;
+import com.ch.team.service.TeamException;
 
 /**
  * 
@@ -19,6 +20,18 @@ public class NameListServiceTest {
 		Employee[] employees = service.getAllEmployees();
 		for (int i = 0; i < employees.length; i++) {
 			System.out.println(employees[i]);
+		}
+	}
+
+	@Test
+	public void testgetEmploy() {
+		NameListService service = new NameListService();
+		int id = 15;
+		try {
+			Employee employee = service.getEmploy(id);
+			System.out.println(employee);
+		} catch (TeamException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
